a11115f206524bfab69dc53d4717ec38a0d00577
jvsena42 <jvsena16@gmail.com>
2021-02-19 11:29:01 -0300

    [NEW] movie datasource

--- app/src/main/java/com/anushka/tmdbclient/data/db/TVShowDao.kt
+++ app/src/main/java/com/anushka/tmdbclient/data/db/TVShowDao.kt
@@ -9,11 +9,11 @@ import com.anushka.tmdbclient.data.model.tvshow.TvShow
 @Dao
 interface TVShowDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun saveTVShows(tvShow: TvShow)
+    suspend fun saveTVShows(tvShow: List<TvShow>)
 
     @Query("DELETE FROM popular_tvShow")
     suspend fun deleteAllTVShows()
 
     @Query("SELECT * FROM popular_tvShow")
-    suspend fun getTVShows(tvShow: TvShow)
+    suspend fun getTVShows( ): List<TvShow>
 }
---
+++ app/src/main/java/com/anushka/tmdbclient/data/repository/movie/MovieRepositoryImpl.kt
@@ -0,0 +1,83 @@
+package com.anushka.tmdbclient.data.repository.movie
+
+import android.util.Log
+import com.anushka.tmdbclient.data.domain.repository.MovieRepository
+import com.anushka.tmdbclient.data.model.movie.Movie
+import com.anushka.tmdbclient.data.repository.movie.datasource.MovieCacheDataSource
+import com.anushka.tmdbclient.data.repository.movie.datasource.MovieLocalDataSource
+import com.anushka.tmdbclient.data.repository.movie.datasource.MovieRemoteDataSource
+import java.lang.Exception
+
+class MovieRepositoryImpl(
+    private val movieRemoteDataSource: MovieRemoteDataSource,
+    private val movieLocalDataSource: MovieLocalDataSource,
+    private val movieCacheDataSource: MovieCacheDataSource
+): MovieRepository {
+    override suspend fun getMovies(): List<Movie>? {
+        //Cache -> Banco local - API
+        return getMoviesFromCache()
+    }
+
+    override suspend fun updateMovies(): List<Movie>? {
+        val newLisOfMovies = getMoviesFromAPI()
+        movieLocalDataSource.clearAll()
+        movieLocalDataSource.saveMoviesToDB(newLisOfMovies)
+        movieCacheDataSource.saveMoviesToCache(newLisOfMovies)
+        return newLisOfMovies
+    }
+
+    private suspend fun getMoviesFromAPI():List<Movie>{
+        lateinit var movieList:List<Movie>
+        try {
+            val response = movieRemoteDataSource.getMovies()
+            val body = response.body()
+            if (body != null){
+                movieList = body.movies
+            }
+        }catch (exception:Exception){
+            Log.i("MyTag", exception.message.toString())
+        }
+
+        return movieList
+    }
+
+    private suspend fun getMoviesFromDB():List<Movie>{
+        lateinit var movieList:List<Movie>
+
+        try {
+            movieList = movieLocalDataSource.getMoviesFromDB()
+        }catch (exception: Exception){
+            Log.i("MyTag", exception.message.toString())
+        }
+
+        if (movieList.isNotEmpty()){
+            //Se a lista não for vazia, retorna ela
+            return movieList
+        }else{
+            //Se a lista for vazia, recupera ela da API e salva no banco local
+            movieList = getMoviesFromAPI()
+            movieLocalDataSource.saveMoviesToDB(movieList)
+        }
+        return movieList
+    }
+
+    private suspend fun getMoviesFromCache():List<Movie>{
+        lateinit var movieList:List<Movie>
+
+        try {
+            movieList = movieCacheDataSource.getMoviesFromCache()
+        }catch (exception: Exception){
+            Log.i("MyTag", exception.message.toString())
+        }
+
+        if (movieList.isNotEmpty()){
+            //Se a lista não for vazia, retorna ela
+            return movieList
+        }else{
+            //Se a lista for vazia, recupera ela do DB e salva no cache
+            movieList = getMoviesFromDB()
+            movieCacheDataSource.saveMoviesToCache(movieList)
+        }
+        return movieList
+    }
+}
